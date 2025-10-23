c2e74c93e4a0bd93975938ba11a132bdf991c008
Jack WebDev <jackbuda31@gmail.com>
2025-10-23 07:28:55 +0200

    update vercel handler function

--- apps/server/api/[[...route]].ts
+++ apps/server/api/[[...route]].ts
@@ -1,5 +1,6 @@
 import { handle } from "hono/vercel";
 import { app } from "../src/app";
 
-export const config = { runtime: "nodejs18.x" };
+export const runtime = "nodejs";
+
 export default handle(app);
